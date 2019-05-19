b6600004c29523b2564031443dfcf3be6dbaf52c
Dan Broadbent <utmorganizerbusiness@gmail.com>
2019-05-19 09:26:33 -0600

    Add start screen

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -15,14 +15,18 @@ class Board extends React.Component {
         ["x", "x", "x", "x", "x", "x"],
         ["x", "x", "x", "x", "x", "x"],
       ],
-      redTurn: false
+      redTurn: false,
+      newGame: true
     }
   }
 
   renderColumns() {
     return this.state.grid.map((column, i) => {
       return(
-        <div className="column">
+        <div 
+          key={`column-${i}`}
+          className="column"
+        >
           {this.renderCells(column, i)}
         </div>
       )
@@ -30,17 +34,34 @@ class Board extends React.Component {
   }
 
   renderCells(column, columnNumber) {
-    return column.map(cell => {
+    return column.map((cell, i) => {
       return(
-        <Cell value={this.state.grid[columnNumber, cell]} />
+        <Cell
+          key={`column-${columnNumber}-row-${i}`}
+          value={cell} 
+        />
       )
     })
   }
 
   render() {
+    if (this.state.newGame) {
+      const setInitialState = (color) => this.setState({ newGame: false, redTurn: color === 'red' ? true : false})
+      return (
+        <div>
+          Player 1 - Please Choose Your Color:
+          <div>
+            <div onClick={() => setInitialState('red')}>Red</div>
+            <br ></br>
+            <div onClick={() => setInitialState('black')}>Black</div>
+          </div>
+        </div>
+      )
+    }
+    const playerTurn = this.state.redTurn ? 'Red' : 'Black';
     return (
       <div>
-        <div>color Player - TAKE TURN</div>
+        <div>{playerTurn} Player - TAKE TURN</div>
         <div className="board">
           {this.renderColumns()}
         </div>
